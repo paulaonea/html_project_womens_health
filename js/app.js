e9161f8e5fd9323bedf44ceabd7997bed42e4580
Paula Onea <paula.onea@gmail.com>
2019-11-23 13:04:37 +0000

    added app.js to all pages.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
+
+document.addEventListener('DOMContentLoaded', function() {
 let subscribe = document.getElementById('submit');
+console.log(subscribe);
 subscribe.addEventListener('click', function(event){
     alert("Thank you for subscribing to our newsletter!");
+});
 })
