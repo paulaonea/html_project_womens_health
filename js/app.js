0184329cdfe8b3949e23b41e6316d196f42f1a71
Paula Onea <paula.onea@gmail.com>
2019-11-23 22:09:21 +0000

    made menu media responsive on all pages

--- js/app.js
+++ js/app.js
@@ -5,4 +5,12 @@ console.log(subscribe);
 subscribe.addEventListener('click', function(event){
     alert("Thank you for subscribing to our newsletter!");
 });
+
+function menuToggle() {
+    document.querySelector('.nav2').classList.toggle('active');
+    }
+
+    document.querySelector('.menu-icon').addEventListener('click', menuToggle);
+    document.querySelector('.cross').addEventListener('click', menuToggle);
+
 })
